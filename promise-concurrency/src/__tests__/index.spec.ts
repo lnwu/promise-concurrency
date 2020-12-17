0a86f3039b1488517cd220569064af7f82ec3038
lnwu <wind2729@gmail.com>
2020-12-17 20:51:16 +0800

    restore timer after each test

--- promise-concurrency/src/__tests__/index.spec.ts
+++ promise-concurrency/src/__tests__/index.spec.ts
@@ -4,7 +4,17 @@ import { defer } from "../defer";
 
 import { PromiseConcurrencyController } from "../index";
 
-const test = ava;
+const test = ava as TestInterface<{
+  timer: Sinon.SinonFakeTimers;
+}>;
+
+test.beforeEach((t) => {
+  t.context.timer = Sinon.useFakeTimers();
+});
+
+test.afterEach((t) => {
+  t.context.timer.restore();
+});
 
 export const generateFixtureTask = <T>(defer: number, resolvedValue: T) => {
   return () =>
@@ -14,7 +24,6 @@ export const generateFixtureTask = <T>(defer: number, resolvedValue: T) => {
 };
 
 test("should be able to run all tasks", async (t) => {
-  const timer = Sinon.useFakeTimers();
   const taskNames = ["task1", "task2"];
 
   const task1 = generateFixtureTask(1000, taskNames[0]);
@@ -37,12 +46,11 @@ test("should be able to run all tasks", async (t) => {
   const d = defer<void>();
   runSpec();
 
-  timer.tickAsync(2000);
+  t.context.timer.tickAsync(2000);
   return d.promise;
 });
 
-test("should task 3000ms when concurrency set to 1", async (t) => {
-  const timer = Sinon.useFakeTimers();
+test("tasks should take 3000ms when concurrency set to 1", async (t) => {
   const taskNames = ["task1", "task2"];
 
   const task1 = generateFixtureTask(1000, taskNames[0]);
@@ -63,7 +71,34 @@ test("should task 3000ms when concurrency set to 1", async (t) => {
   }
 
   const d = defer<void>();
-  await timer.tickAsync(3000);
+  await t.context.timer.tickAsync(3000);
+
+  runSpec();
+  return d.promise;
+});
+
+test("should run tasks by concurrency set when call run multiply times", async (t) => {
+  const taskNames = ["task1", "task2"];
+
+  const task1 = generateFixtureTask(1000, taskNames[0]);
+  const task2 = generateFixtureTask(2000, taskNames[1]);
+
+  const controller = new PromiseConcurrencyController<string>(1);
+
+  const result = controller.run(task1, task2);
+
+  async function runSpec() {
+    let index = 0;
+    for await (const value of result) {
+      t.is(value, taskNames[index]);
+      index++;
+    }
+    t.is(index, taskNames.length);
+    d.resolve();
+  }
+
+  const d = defer<void>();
+  await t.context.timer.tickAsync(3000);
 
   runSpec();
   return d.promise;
